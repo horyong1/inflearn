9beb3f03d8ea7a1fe243d0edb2f7148aaa8a21f2
potato <kmn0848@naver.com>
2023-09-20 21:15:26 +0900

    API메시지바디-TEXT

---
+++ JSPservlet/servlet/src/main/java/hello/servlet/basic/request/RequestBodyStringServlet.java
@@ -0,0 +1,31 @@
+package hello.servlet.basic.request;
+
+import org.springframework.util.StreamUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+@WebServlet(name = "requestBodyStringServlet", urlPatterns = "/request-body-string")
+public class RequestBodyStringServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        //메세지 바디 내용을 바이트 코드로  얻을 수 있음
+        ServletInputStream inputStream = request.getInputStream();
+        //바이트 코드 -> String으로 변경(항상 문자 변경시 인코딩 정보 알려줘야함)
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+        System.out.println("messageBody = " + messageBody);
+
+        response.getWriter().write("ok");
+
+    }
+}
