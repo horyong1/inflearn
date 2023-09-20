6678b7c2ca3369cad22e2c9f727a08e68cbb9c32
potato <kmn0848@naver.com>
2023-09-20 21:04:49 +0900

    클래스명 변경

--- JSPservlet/servlet/src/main/java/hello/servlet/basic/request/RequestParamServlet.java
+++ JSPservlet/servlet/src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -6,7 +6,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Enumeration;
 
 
 /*
@@ -14,15 +13,14 @@ import java.util.Enumeration;
 * http://localhost:8080/request-param?username=hello&age=20
 * */
 
-@WebServlet(name="requestRaramServlet",urlPatterns = "/request-param")
-public class RequestRaramServlet extends HttpServlet{
+@WebServlet(name="requestParamServlet",urlPatterns = "/request-param")
+public class RequestParamServlet extends HttpServlet{
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // 전체 파라미터 조회방법
         System.out.println("[전체 파라미터 조회] - start");
         request.getParameterNames().asIterator()
                         .forEachRemaining(paramName -> System.out.println(paramName+": "+request.getParameter(paramName)));
-        System.out.println("[전체 파라미터 조회] - end");
         System.out.println();
 
         // 단일 파라미터 조회방법
@@ -31,7 +29,6 @@ public class RequestRaramServlet extends HttpServlet{
         System.out.println("username = " + username);
         String age = request.getParameter("age");
         System.out.println("age = " + age);
-        System.out.println("[단일 파라미터 조회] - end");
         System.out.println();
 
         // 이름이 같은 복수 파라미터 조회방법
@@ -41,6 +38,7 @@ public class RequestRaramServlet extends HttpServlet{
         // 중복일때 request.getParameter() 사용하면
         // request.getParameterValues() 첫번째 값이 반환됨 (중복으로 보내는 일은 거의없음)
         String [] usernames = request.getParameterValues("username");
+
         // iter 입력하면 아래와 같이 입력됨
         for (String name : usernames) {
             System.out.println("username = " + name);
