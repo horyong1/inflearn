1056822e4b4c5bccda7f006c03fab67754c49208
potato <kmn0848@naver.com>
2023-10-16 21:41:56 +0900

    View 분리 -V2

---
+++ JSPservlet/servlet/src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -0,0 +1,22 @@
+package hello.servlet.web.frontcontroller;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.rowset.serial.SerialException;
+import java.io.IOException;
+import java.rmi.server.ServerCloneException;
+
+public class MyView {
+    private String viewPath;
+
+    public MyView(String viewPath) {
+        this.viewPath = viewPath;
+    }
+
+    public void render(HttpServletRequest request, HttpServletResponse response) throws  SerialException, IOException, ServletException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request,response);
+    }
+}
